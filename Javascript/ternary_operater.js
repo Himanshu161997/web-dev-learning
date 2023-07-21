bfff4568f72701743f85f47810b82689ea53c1b8
Himanshu <himanshu.rajender@gmail.com>
2023-07-21 18:54:50 -0400

    condition statements

---
+++ Javascript/ternary_operater.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")({sigint:true});
+let age = Number(prompt("what is ypur age:"));
+
+const drive = age>=18 ? "you can drive" : "you cannot drive";
+console.log(drive);
