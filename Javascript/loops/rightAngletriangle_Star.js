1b3b2a931c9ee1c0a71989045d41b7609535bcde
Himanshu <himanshu.rajender@gmail.com>
2023-07-28 12:24:45 -0400

    star right angle

---
+++ Javascript/loops/rightAngletriangle_Star.js
@@ -0,0 +1,10 @@
+const size = 5;
+let output = "";
+
+for (let index = 1; index <= size; index++) {
+  for (j = 1; j <= index; j++) {
+    output += "*";
+  }
+  console.log(output);
+  output = "";
+}
