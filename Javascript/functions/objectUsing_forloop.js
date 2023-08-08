a067b1456c2a1a4dfd756e796919e78fb8c060a3
Himanshu <himanshu.rajender@gmail.com>
2023-08-08 12:25:19 -0400

    changed name of the file

--- Javascript/functions/objectUsing_forloop.js
+++ Javascript/functions/objectUsing_forloop.js
@@ -7,3 +7,4 @@ for(let i = 0; i<Object.keys(marks).length;i++){
     console.log("the marks of " + Object.keys(marks)[i] + "are" +  marks[Object.keys(marks)[i]]);
 }
 //print marks of student in object using foor loop
+
