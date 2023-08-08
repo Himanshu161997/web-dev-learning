673c10abd6133f4ca1ae2a86f476bb94d4aec695
Himanshu <himanshu.rajender@gmail.com>
2023-08-08 16:24:56 -0400

    arrow function

--- Javascript/functions/functionAs_expression.js
+++ Javascript/functions/functionAs_expression.js
@@ -1,5 +1,6 @@
-let add = function (num1,num2) {
-    return num1 + num2;
-}
+let add =(num1,num2) => 
+     num1 + num2;
+
 let result = add(5,6);
 console.log(result);
+// while using arrow function if we have only one statement then we can skip curly brackets and return keyword
