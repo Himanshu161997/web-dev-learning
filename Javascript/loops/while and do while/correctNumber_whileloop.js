f8a4125609a3f05a0f1cf2ef326415c123d4638e
Himanshu <himanshu.rajender@gmail.com>
2023-08-08 13:05:58 -0400

    enter correct number using while loop

---
+++ Javascript/loops/while and do while/correctNumber_whileloop.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")({sigint:true});
+let num = 6;
+let i ;
+while (i!=num) {
+   
+    i = prompt("try again!! enter the number");
+
+}
+console.log("you have entered the correct number");
