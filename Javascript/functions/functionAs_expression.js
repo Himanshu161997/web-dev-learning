a3fbb0d745cd16fbc6727c6800b05988a8325c4e
Himanshu <himanshu.rajender@gmail.com>
2023-08-08 16:25:57 -0400

    used arrow function to execute this problem

--- Javascript/functions/functionAs_expression.js
+++ Javascript/functions/functionAs_expression.js
@@ -2,5 +2,6 @@ let add =(num1,num2) =>
      num1 + num2;
 
 let result = add(5,6);
+
 console.log(result);
 // while using arrow function if we have only one statement then we can skip curly brackets and return keyword
