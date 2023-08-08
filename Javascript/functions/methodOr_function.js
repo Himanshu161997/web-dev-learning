d2e87b1c45d65ee9ff72b250e1006ac52c9f01ae
Himanshu <himanshu.rajender@gmail.com>
2023-08-08 16:45:50 -0400

    method or function inside object

---
+++ Javascript/functions/methodOr_function.js
@@ -0,0 +1,11 @@
+let object = {
+    name : 'honey',
+    class : '4',
+    rollnumber: '34',
+
+   fun :function(){
+    console.log("hell");
+   }
+}
+object.fun();
+//we passed function inside  object and call it at the end
