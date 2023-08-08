d69a0894a2f7c19fcb51d26f19eb19526d25847e
Himanshu <himanshu.rajender@gmail.com>
2023-08-08 12:31:07 -0400

    printed  marks in object using forin loop

---
+++ Javascript/objects/objectUsing_forInloop.js
@@ -0,0 +1,9 @@
+let marks = {
+    honey : '5',
+    money : '6',
+    manu : '3',
+}
+for(let key in marks){
+    console.log("the marks of" + key + "are" + marks[key]);
+}
+// print the marks of student in object using for in loop
