3ce535d04d94e3c46ce905708e1938a535687766
Himanshu <himanshu.rajender@gmail.com>
2023-08-13 11:34:57 -0400

    first array program

---
+++ Javascript/array.js
@@ -0,0 +1,6 @@
+//creating array and assining different type of data type
+//numbers.string,object,function then calling them with console or index value
+
+let values = [3,'honey',{jem:'ho'},function() {console.log("hello")}]
+console.log(values);
+values[3]();
