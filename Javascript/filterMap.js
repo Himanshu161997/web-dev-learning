b53b72cbdb322c53b4ceac29fc8fc06900f8c946
Himanshu <himanshu.rajender@gmail.com>
2023-08-13 13:25:54 -0400

    filtermap method in js

---
+++ Javascript/filterMap.js
@@ -0,0 +1,7 @@
+let num = [3,24,34,67,89]
+//use filter method to filter even number 
+num.filter(n  => n%2===0)
+//we use foreach loop in array to arrange the numbers one by one
+.forEach(n =>{
+    console.log(n)
+})
