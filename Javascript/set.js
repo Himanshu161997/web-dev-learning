27b33a8996a804e61997a9655696505c1bc48de2
Himanshu <himanshu.rajender@gmail.com>
2023-08-13 14:07:36 -0400

    set and has method

---
+++ Javascript/set.js
@@ -0,0 +1,12 @@
+let num = new Set();
+
+num.add(3);
+num.add(5);
+num.add(6);
+num.add('hony');
+num.add("sunny");
+// num.forEach(num => {
+//     console.log(num);
+    
+// })
+console.log(num.has('hony'))
