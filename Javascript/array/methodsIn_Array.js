f40af4b8353e3a29e3389b62d6e481e975f77ad3
Himanshu <himanshu.rajender@gmail.com>
2023-08-13 12:08:41 -0400

    shift,pop methods

---
+++ Javascript/array/methodsIn_Array.js
@@ -0,0 +1,9 @@
+let data =[2,3,4,5];
+
+console.log(data.push(10))
+console.log(data)
+console.log(data.pop());
+console.log(data.shift());
+console.log(data);
+//shift method will remove from the start
+//pop method will remove from the end-> lifo
