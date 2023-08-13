ca898b3ca60aba9e0080b76dc304b5b08e52ad1b
Himanshu <himanshu.rajender@gmail.com>
2023-08-13 13:55:50 -0400

    filter,map,reduce method in js

--- Javascript/filterMap.js
+++ Javascript/filterMap.js
@@ -1,7 +1,10 @@
 let num = [3,24,34,67,89]
 //use filter method to filter even number 
-num.filter(n  => n%2===0)
+let result = num.filter(n  => n%2===0)
+//now we use map method to take the method and change it or double it
+.map(n =>n*2)
+//now we will use reduce method to take some value and reduce it to one value 
+.reduce((a,b) => a+b);
 //we use foreach loop in array to arrange the numbers one by one
-.forEach(n =>{
-    console.log(n)
-})
+// .forEach(n =>{
+    console.log(result)
