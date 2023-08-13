964c98c9705d697f88136eda3d53df4546e925f0
Himanshu <himanshu.rajender@gmail.com>
2023-08-13 12:36:08 -0400

    for off loop

---
+++ Javascript/for_OFFloop .js
@@ -0,0 +1,15 @@
+let nums =[];
+nums[0]= 1;
+nums[10]= 9;
+
+for (const n of nums) {
+    console.log(n);
+
+}
+
+//for in loop is used mostly in objects to fetch the value using key
+//while for off loop is used in array -> when can use for in in array but the we have to give key that is lenthy process
+// for (const key in nums) {
+    
+//        console.log(nums[key]) 
+//     }
