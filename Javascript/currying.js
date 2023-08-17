4793bc67db7b18bc3b6648e59f0e1c36ed37e52c
Himanshu <himanshu.rajender@gmail.com>
2023-08-17 11:19:08 -0400

    currying function by arrow function

--- Javascript/currying.js
+++ Javascript/currying.js
@@ -6,12 +6,18 @@
 // console.log(add(2,3,5));
 
 //this id the way to do currying function for add 3 numbers
-function add(a) {
-    return function(b){
-        return function (c) {
-            return a+b+c;
+// function add(a) {
+//     return function(b){
+//         return function (c) {
+//             return a+b+c;
             
-        };
-    };
-};
-console.log(add(1)(2)(3))
+//         };
+//     };
+// };
+// console.log(add(1)(2)(3))
+
+//this is the way to do this program by ecmascript 6 
+//by arrow function
+
+let add = (a)  => (b) => (c) => a+b+c;
+console.log(add(1)(2)(4));
